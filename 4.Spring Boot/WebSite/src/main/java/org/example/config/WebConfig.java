7476b7ebac6d51a0d4fcde5a785c6569dd480a58
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-11-07 20:53:08 +0200

    Working is good

--- 4.Spring Boot/WebSite/src/main/java/org/example/config/WebConfig.java
+++ 4.Spring Boot/WebSite/src/main/java/org/example/config/WebConfig.java
@@ -1,5 +1,7 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${upload.dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Serve /uploads/** URLs from the filesystem "uploads" folder
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/"+uploadDir+"/**")
+                .addResourceLocations("file:"+uploadDir+"/");
     }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
 }
--- 4.Spring Boot/WebSite/src/main/java/org/example/controllers/AccountController.java
+++ 4.Spring Boot/WebSite/src/main/java/org/example/controllers/AccountController.java
@@ -1,15 +1,21 @@
 package org.example.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.example.services.AccountService;
+import org.example.services.FileService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final FileService fileService;
 
     @GetMapping("/users")
     public String listUsers(Model model) {
@@ -22,9 +28,24 @@ public class AccountController {
         return "account/register";
     }
 
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String username,
+                               @RequestParam String password,
+                               @RequestParam(required = false) MultipartFile imageFile,
+                               HttpServletRequest request,
+                               Model model) {
 
+        String fileName = fileService.load(imageFile);
 
+        boolean success = accountService.registerUser(username, password, fileName);
+        if (success) {
+            model.addAttribute("message", "Реєстрація успішна!");
 
+            return "redirect:/users";
+        } else {
+            model.addAttribute("message", "Користувач із таким іменем вже існує.");
+        }
 
-
+        return "account/register";
+    }
 }
