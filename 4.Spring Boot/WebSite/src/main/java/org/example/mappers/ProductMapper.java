592055fd764ea4e838acfd143778b6ce18369d67
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-11-11 20:01:19 +0200

    Add Category and Product table

---
+++ 4.Spring Boot/WebSite/src/main/java/org/example/mappers/ProductMapper.java
@@ -0,0 +1,13 @@
+package org.example.mappers;
+
+import org.example.data.seed.CategorySeed;
+import org.example.data.seed.ProductSeed;
+import org.example.entities.ProductEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    @Mapping(target = "images", ignore = true)
+    ProductEntity toEntity(ProductSeed product);
+}
